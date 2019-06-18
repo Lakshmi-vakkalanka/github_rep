76bfb6f1058b63b41a3457490f36b2e18d3ad750
unknown <lsathiraju@gmail.com>
2019-06-19 00:08:11 +0530

    My first Git upload

--- MyFirstCapstoneProj.py
+++ MyFirstCapstoneProj.py
@@ -1,4 +1,4 @@
-### This script will be used mainly for Capstone Project
+<h2> This script will be used mainly for Capstone Project <h2/>
 import pandas as pd
 import numpy as np
 print ("Hello Capstone Project Course!")
